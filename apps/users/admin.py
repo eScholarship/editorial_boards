e9cac119e7f394ef6a4294b9702f83f2ba3daea7
Esther Verreau <esther.verreau@ucop.edu>
2025-10-31 11:38:23 -0500

    customize user admin for email only users

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import CustomUser
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'is_staff', 'is_active', 'date_joined')
     search_fields = ('email',)
     list_filter = ('is_staff', 'is_active')
+    ordering = ('email',)
+    exclude = ('username',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', "password1", "password2")}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    def response_add(self, request, obj, post_url_continue=None):
+        changelist_url = reverse(f'admin:{obj._meta.app_label}_{obj._meta.model_name}_changelist')
+        return HttpResponseRedirect(changelist_url)
